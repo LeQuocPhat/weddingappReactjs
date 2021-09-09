1f7ff651894c9b41567ff219e186efd891748196
NguyenHuuKiet15 <1851050072kiet@ou.edu.vn>
2021-09-09 09:50:49 +0700

    commit on main

--- src/A_login.js
+++ src/A_login.js
@@ -1,46 +1,35 @@
-import  React,  { useState } from "react"
+import  React,  { useContext, useState } from "react"
 import { Form, Button } from "react-bootstrap"
-import API, { AuthAPI, endpoints } from "./API"
-import cookies from 'react-cookies'
+import { Redirect } from "react-router"
+import { UserContext } from "./App"
+// import API, { AuthAPI, endpoints } from "./API"
+// import cookies from 'react-cookies'
 
 export default function A_login(props) {
     const [username, setUsername] = useState(null)
     const [password, setPassword] = useState(null)
+    const [isLogged, setLogged] = useState(false)
+    const auth = useContext(UserContext)
 
     const login = async (event) => {
         event.preventDefault()
-
-        let res = await API.post(endpoints['login'], {
-            'client_id':'FtaRWM9Nb53WSKZVlfzIpHufui2DvY7TADNwe8fy',
-            'client_secret':'Lnai9ViEGYyVSr5Nq3lvqThtb7W8BQfvdaj4bZMoFOROYx53hktZLExOwNTnTnt3sx1tjv13Q50qRYRFITgmCMH5ybCavRl3NjWIXz4pTu3shQNWEhKCYjwBo9UpGWMx',
-            'username':username,
-            'password':password,
-            'grant_type':'password'
-        })
-        console.info(res.data)
-        
-        cookies.save("access_token", res.data.access_token)
-
-        let user = await API.get(endpoints['current-user'], {
-            headers: {
-                'Authorization': `Bearer ${cookies.load('access_token')}`
-            }
-        })
-        console.info(user.data)
-        cookies.save("user", user.data)
-
-        props.history.push("/")
+        auth.login(username, password)
+        setLogged(true)
     }
-    return (
-        <>
-            <h1 class="text-center text-danger">ĐĂNG NHẬP</h1>
-            <Form onSubmit={login}>
-                <LoginForm id="username" type="text" field={username} change={event => setUsername(event.target.value)}/>
-                <LoginForm id="password" type="password" field={password} change={event => setPassword(event.target.value)}/>
-                <Button type="submit">Login</Button>
-            </Form>
-        </>
-    )
+
+    if(isLogged)
+        return <Redirect to="/" />
+    else
+        return (
+            <>
+                <h1 class="text-center text-danger">ĐĂNG NHẬP</h1>
+                <Form onSubmit={login}>
+                    <LoginForm id="username" type="text" field={username} change={event => setUsername(event.target.value)}/>
+                    <LoginForm id="password" type="password" field={password} change={event => setPassword(event.target.value)}/>
+                    <Button type="submit">Login</Button>
+                </Form>
+            </>
+        )
 }
 
 class LoginForm extends React.Component {
--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import React, { useState } from 'react';
 import A_about from './A_about';
-import React from 'react';
 import A_header from './A_header';
 import A_URL from './A_URL';
+import API, { endpoints } from './API';
+import cookies from 'react-cookies';
 
-class App extends React.Component {
-  render() {
-    return (
+
+export let UserContext = React.createContext() 
+
+export default function App(props) {
+  const [user, setUser] = useState(null)
+
+  const login = async (username, password) => {
+    let res = await API.post(endpoints['login'], {
+        'client_id':'FtaRWM9Nb53WSKZVlfzIpHufui2DvY7TADNwe8fy',
+        'client_secret':'Lnai9ViEGYyVSr5Nq3lvqThtb7W8BQfvdaj4bZMoFOROYx53hktZLExOwNTnTnt3sx1tjv13Q50qRYRFITgmCMH5ybCavRl3NjWIXz4pTu3shQNWEhKCYjwBo9UpGWMx',
+        'username':username,
+        'password':password,
+        'grant_type':'password'
+    })
+    console.info(res.data)
+    
+    cookies.save("access_token", res.data.access_token)
+
+    let user = await API.get(endpoints['current-user'], {
+        headers: {
+            'Authorization': `Bearer ${cookies.load('access_token')}`
+        }
+    })
+    console.info(user.data)
+    cookies.save("user", user.data)
+
+    setUser(user)
+
+  }
+
+  return (
+    <UserContext.Provider value={{user, login}}>
       <BrowserRouter>
         <div>
-        <A_header/>
+          <A_header/>
           <A_about/>
           <A_URL/>
         </div>
       </BrowserRouter>
-    )
-  }
+    </UserContext.Provider>
+  )
 }
-
-export default App;
